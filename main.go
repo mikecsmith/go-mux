043bde2fe3ce6b86a3723adba361efe9048b3ead
Michael Smith <mike@cadmia.io>
2021-04-10 16:45:50 +0100

    update main.go

--- main.go
+++ main.go
@@ -1,1 +1,12 @@
-package go_mux
+package main
+
+import "os"
+
+func main() {
+	a := App{}
+	a.Initialize(
+		os.Getenv("APP_DB_USERNAME"),
+		os.Getenv("APP_DB_PASSWORD"),
+		os.Getenv("APP_DB_NAME"))
+	a.Run(":8010")
+}
