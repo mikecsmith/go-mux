d1b3142ba0647fc0c1ab374710ded78232a58c20
Michael Smith <mike@cadmia.io>
2021-04-10 16:46:56 +0100

    Add App struct, init and run funcs

--- app.go
+++ app.go
@@ -1,1 +1,20 @@
-package go_mux
+package main
+
+import (
+	"database/sql"
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+type App struct {
+	Router *mux.Router
+	DB     *sql.DB
+}
+
+func (a *App) Initialize(user, password, dbname string) {
+
+}
+
+func (a *App) Run(addr string) {
+
+}
